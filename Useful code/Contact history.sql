498747454d3284819689c47ab2a2273ba4aa81b0
Sacha.Hawker <sacha.hawker@sainsburys.co.uk>
2022-06-09 11:15:06 +0100

    Update

---
+++ Useful code/Contact history.sql
@@ -0,0 +1,23 @@
+-- CONTACT HISTORY TABLE --
+-- Campaign name will be; year_TDXX e.g. 2223_TD02
+-- Flowchartname will be: the campaign e.g. Standard_DM or Flash_Email
+Select * from EDWS_PROD.PROD_CMT_PRESENTATION.vw_chrh_campaign_ec as a
+inner join EDWS_PROD.PROD_CMT_PRESENTATION.vw_ch_fact_ec b
+on a.flowchartid = b.flowchartid
+inner join EDWS_PROD.PROD_CMT_PRESENTATION.vw_ch_offer_attribute_ec as c
+on b.treatmentcode = c.treatmentcode
+left join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_PARTY_ACCOUNT as ec
+on ec.PARTY_ACCOUNT_ID = b.SR_ID
+left join (select distinct trade_driver,
+campaign_type,
+campaign,
+flowchartname from td_date_and_campaign_map_SH) as td
+on a.flowchartname = td.flowchartname
+and a.campaignname = td.trade_driver
+where a.campaignname = '2223_TD02'
+and a.flowchartname = 'Flash_Email'
+
+
+
+
+
