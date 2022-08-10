38360d8ed991c4e82e6e59f1e2eaadfda73a33d4
Sacha.Hawker <sacha.hawker@sainsburys.co.uk>
2022-08-10 15:59:38 +0100

    Updated

--- Projects/Bank Credit Card.sql
+++ Projects/Bank Credit Card.sql
@@ -3,12 +3,6 @@ USE WAREHOUSE WHS_PROD_MARKETING_ANALYTICS_LARGE;
 USE DATABASE CUSTOMER_ANALYTICS;
 USE SCHEMA TD_REPORTING;
 
-Select * from EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES limit 5;
-
-select count (enterprise_customer_id) from EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES;
-
-select * from Bank_credit_card limit 5;
-
 create or replace temp table ranking_file as
     select
           substr(enterprise_customer_ID,4,11) as ec_id,
@@ -17,34 +11,43 @@ create or replace temp table ranking_file as
            segment
 from EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES;
 
+
 Create or replace temp table Card_customers as
     select
-           a.ec_id,
-           a.party_account_id,
            a.party_account_no,
            a.segment,
            b.hashed_loyalty_id
-from ranking_file as a
+from EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES as a
 inner join bank_credit_card as b
-on sha2(cast(a.ec_id as varchar(50)), 256) = b.hashed_loyalty_id;
+on sha2(cast(a.party_account_no as varchar(50)), 256) = b.hashed_loyalty_id;
 
-select count(distinct ec_id) from Card_customers;
--- those in CC data that are in the ranking file: 129
+
+select count(*)
+from "EDWS_PROD"."PROD_CMT_PRESENTATION"."VW_CA_CUSTOMER_ACCOUNT"  as a
+inner join bank_credit_card as b
+on b.hashed_loyalty_ID = sha2(cast(a.party_account_no as varchar(50)), 256)
+
+select * from "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."RANKING_FILE_SR_202221";
+
+select count(distinct party_account_no) from Card_customers;
+-- those in CC data that are in CURRENT the ranking file: 511,633
+-- those in CC data that are in LAST WEEKS ranking file: 511,709
 
 select count(distinct hashed_loyalty_id) from bank_credit_card;
 -- 744,299
 
-select count(ec_id), segment from Card_customers group by 2;
--- 38,Regular
--- 5,SuperFrequent
--- 1,Missing
--- 33,Infrequent
--- 20,Frequent
--- 14,Lapsed
--- 18,Inactive
+-- Matching bank card on customer account: 744,082 -- 217 missing -- deactivated bank/nectar?
+
+select count(party_account_no), segment from Card_customers group by 2;
+-- 151282,Regular
+-- 34097,SuperFrequent
+-- 11513,Missing
+-- 122479,Infrequent
+-- 106808,Frequent
+-- 45486,Lapsed
+-- 39968,Inactive
 
 
-select ec_id,
-       sha2(cast(ec_id as varchar(50)), 256) as hashed,
-       segment
-       from ranking_file;
+
+
+
