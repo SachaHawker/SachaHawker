16369ec47daa7361ea9fc75f04080136c5e39e68
Sacha.Hawker <sacha.hawker@sainsburys.co.uk>
2022-08-19 10:45:39 +0100

    Updated

--- Useful code/Tables.sql
+++ Useful code/Tables.sql
@@ -1,3 +1,5 @@
 select * from EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.vw_payment_line; -- to get all payment info, has party account id (SR_ID)
 
 select * from BARCODE_LOOKUP; -- info on barcodes, offers, dates
+
+Select * from EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES -- Ranking file
