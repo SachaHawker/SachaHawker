0a7187d2e4d584403a845e85278ed074d572f3b4
Sacha.Hawker <sacha.hawker@sainsburys.co.uk>
2022-11-09 19:51:57 +0000

    Updated tracking for dashboard

--- Projects/Bank Credit Card Analysis/Bank Script for dashboard.sql
+++ Projects/Bank Credit Card Analysis/Bank Script for dashboard.sql
@@ -3,9 +3,10 @@ USE WAREHOUSE WHS_PROD_MARKETING_ANALYTICS_LARGE;
 USE DATABASE CUSTOMER_ANALYTICS;
 USE SCHEMA TD_REPORTING;
 
--- Draw out SR_ID and full nectar card no. associated with hashed loyalty ID
+//Find SR_ID and full nectar card no. associated with hashed loyalty ID from bank table
+-- This is only for Gold and Platnium customers with 10k or over points in last 12m and have an instore account
 create or replace temp table bank_base as
-    select a.SR_ID, b.hashed_loyalty_id, FULL_NECTAR_CARD_NUM, ec_id
+    select a.SR_ID, b.hashed_loyalty_id, FULL_NECTAR_CARD_NUM, ec_id, a.party_account_type_code
         from "CUSTOMER_ANALYTICS"."DEVELOPMENT"."BANK_CREDIT_CARD_CUSTOMERS_WITH_FLAG" as b
         left join EDWS_PROd.PROD_CMT_PRESENTATION.VW_CA_CUSTOMER_ACCOUNT as a
             on sha2(cast(a.party_account_no as varchar(50)), 256) = b.hashed_loyalty_id
@@ -15,212 +16,290 @@ select * from bank_base;
 
 -- Check they all match
 select SR_ID from bank_base order by 1;
-select count(*), count(distinct SR_ID), count(distinct hashed_loyalty_id) from bank_base;
+select count(*), count(distinct ec_id), count(distinct hashed_loyalty_id) from bank_base;
 --matching 19338
 
---STEP 2: CREATING JOINED TABLES (BANK CUSTOMERS AND RANKING FILE)//
--- This is only for Gold and Platnium customers with 10k or over points in last 12m and have an instore account //
+
+//Find all customers that are in the ranking file and add flag
 Create or replace temp table Bank_card_customers_in_RF as
     select
            a.sr_id,
-           b.enterprise_customer_ID,
-           b.segment,
+           a.ec_id,
            a.hashed_loyalty_id,
-           b.instore_accounts,
-           b.online_accounts,
            c.journey_instore,
-           b.party_account_type_code,
-           case when b.party_account_id is not null then 1
-               when b.party_account_id is null then 0 end as Ranking_File_Flag
+           a.party_account_type_code,
+           a.FULL_NECTAR_CARD_NUM,
+           case when ec_id in (select enterprise_customer_ID from EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES) then 1
+               else 0
+               end as Ranking_File_Flag
 from bank_base as a
-left outer join EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES as b on b.PARTY_ACCOUNT_ID=a.SR_ID
     left join TD_REPORTING.TD_EXPERIMENTAL_JOURNEYS as c
-    on b.enterprise_customer_ID = c.enterprise_customer_ID;
+    on a.ec_id = c.enterprise_customer_ID;
 
 
-select count (sr_id) from Bank_card_customers_in_RF where Ranking_File_Flag = '1';
+//Finding all customers that are less than 8 weeks active
+create or replace table bank_card_customers_8wk_DN_active1 as
+select *
+from "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_SEGMENTATION" as b
+where weeks_since_latest_activity <='8';
 
-create or replace table bank_card_customers_8wk_DN_active as
-select a.*,
-       b.weeks_since_latest_activity,
-       b.grouped_segmentation,
-       case when b.weeks_since_latest_activity <= '8' then CAST('less_than_8wk' AS varchar)
-           when b.weeks_since_latest_activity > '8' then CAST('over_8wk' AS varchar)
-           end as DN_8_wk_flag
-from Bank_card_customers_in_RF as a
-Left join "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_SEGMENTATION" as b
-on a.enterprise_customer_id = b.ec_id;
-
-
-select count (ec_id) from bank_card_customers_8wk_DN_active where  DN_8_wk_flag = 'less_than_8wk';
-
-
-create or replace temp table bank_card_customers_DN_registered as
-select a.*,
-       b.weeks_since_latest_activity,
-       b.grouped_segmentation
-from Bank_base as a
-inner join "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_SEGMENTATION" as b
-on a.ec_id = b.ec_id;
+//Adding in DN flag to main pot
+create or replace table bank_card_customers_8wk_DN_active2 as
+select *,
+       case when ec_id in (select ec_id from bank_card_customers_8wk_DN_active1) then '8wk Active'
+         else 'Over 8wk Active'
+        end as DN_8_wk_flag
+from Bank_card_customers_in_RF;
 
 
 
-create or replace temp table DN_active_instore as
-select ec_id,
-       '419048' as Campaign_ID
-from bank_card_customers_DN_registered;
-
-create or replace temp table DN_active_online as
-select ec_id,
-       '419047' as Campaign_ID
-from bank_card_customers_DN_registered;
---16,365
-
-Create or replace table DN_active_bank_pot as
-    select ec_id,
-           campaign_id
-from DN_active_instore
-union all
-  select ec_id,
-           campaign_id
-               from DN_active_online
-order by ec_id;
---32,730
-
-
-
-
-create or replace temp table bank_card_customers_transactions as
-select weeks_since_latest_activity,
-       grouped_segmentation,
-       sr_id,
-           enterprise_customer_ID,
-           segment,
-           hashed_loyalty_id,
-           instore_accounts,
-           online_accounts,
-           journey_instore,
-           party_account_type_code,
-       DN_8_wk_flag,
-       Ranking_File_Flag,
-       (select case when max(b.transaction_date) < DATEADD(WEEK, -26, CURRENT_DATE()) then 'more than 26w'
-           when max(b.transaction_date) >= DATEADD(day, -5, CURRENT_DATE()) then 'recent_transaction'
-           else 'probably GOL' end as transaction_flag
-        from bank_card_customers_8wk_DN_active as a
-        inner join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION as b on a.SR_ID=b.PARTY_ACCOUNT_ID
-        where a.ranking_file_flag = 0) as transaction_flag
-from bank_card_customers_8wk_DN_active
-group by 1,2,3,4,5,6,7,8,9,10,11,12;
-
-
-
-
-
-select count(*) from bank_card_customers_transactions where transaction_flag = 'more than 26w';
-
-
-
-
-
--- those that aren't in RF
-create or replace temp table Bank_customers_not_in_RF as
-    select a.*
-    from bank_base a
-    left join Bank_card_customers_in_RF as b on a.SR_ID=b.party_account_id
-    where b.party_account_id is null;
-
-
--- look at the last shop of those that are not in the RF
-create or replace temp table bank_card_not_in_RF_last_shop as
-    select a.SR_ID, max(TRANSACTION_DATE) as last_trx,
-       case when last_trx < DATEADD(WEEK, -26, CURRENT_DATE()) then 'more than 26w'
-           when last_trx >= DATEADD(day, -5, CURRENT_DATE()) then 'recent_transaction'
-    else 'probably GOL' end as flag
-    from Bank_customers_not_in_RF as a
-    inner join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION as b on a.SR_ID=b.PARTY_ACCOUNT_ID
-    group by 1;
-
-
-
--- online only
-create or replace temp table bank_card_GOL as
+// All customers that have shopped online
+Create or replace temp table  bank_card_customers_GOL as
     select a.*, d.SR_ID as GOL_SR_ID, G_26WK_TOTVALUE, G_LASTORDERDATE
-    from Bank_customers_not_in_RF as a
-    left join bank_card_not_in_RF_last_shop c on a.SR_ID = c.SR_ID
+    from bank_card_customers_8wk_DN_active2 as a
     inner join EDWS_PROD.PROD_CMT_PRESENTATION.VW_CA_CUSTOMER_ACCOUNT as d on a.FULL_NECTAR_CARD_NUM=d.FULL_NECTAR_CARD_NUM
     inner join EDWS_PROD.PROD_CMT_PRESENTATION.VW_CA_OL_TRANS_SUMMARY as e on d.SR_ID=e.SR_ID
-    where c.SR_ID is null
-        and d.PARTY_ACCOUNT_TYPE_CODE='02';
+        where d.PARTY_ACCOUNT_TYPE_CODE='02';
+
+--checking numbers still match
+select count (distinct sr_id), count(*) from bank_card_customers_GOL;
+
+//Finding customers that are online//
+create or replace temp table bank_card_customers_GOL_1 as
+    select distinct sr_id, ec_id, sum( G_26WK_TOTVALUE) as total_value
+from bank_card_customers_GOL
+    group by 1,2
+having total_value >100;
+
+//Adding in GOL flag to main pot//
+create or replace temp table bank_card_customers_GOL_2 as
+    select *,
+           case when ec_id in (select ec_id from bank_card_customers_GOL_1) then 'GOL' else 'Not GOL' end as GOL_Flag
+from bank_card_customers_8wk_DN_active2;
 
 
-create or replace temp table bank_card_GOL_1 as
-select case when G_26WK_TOTVALUE > 100 then 'GOL' else 'no' end as gol_flag, *
-from bank_card_GOL;
-
-select * from bank_card_GOL_1;
-
-select count(distinct SR_ID), gol_flag
-
-from bank_card_GOL_1
-group by 2;
 
 
-select * from "CUSTOMER_ANALYTICS"."TD_REPORTING"."SALES_REPORT_REPORTING_HISTORY" limit 10;
 
 //WEEKLY SPEND/VISITS
-create or replace temp table cc_bank_TRANS as
-    select a.SR_ID
+-- By transaction, so can have more than one row per customer
+create or replace table cc_bank_TRANS as
+ select a.SR_ID,
+        a.ec_id
     ,c.week_num
-    , COUNT(DISTINCT TRANSACTION_NUMBER) AS trans
-    , sum(transaction_value) as spend
-    , count(transaction_value) as countspend
-    from bank_base as a
-    inner join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION as b on a.SR_ID=b.PARTY_ACCOUNT_ID
+    , COUNT(DISTINCT concat(b.TRANSACTION_NUMBER, b.transaction_time)) AS trans
+    , sum(b.transaction_value) as spend,
+           DN_8_wk_flag,
+           Ranking_File_Flag,
+           gol_flag
+    from  bank_card_customers_GOL_2 as a
+    left join (select * from EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION  where transaction_date >= '2022-01-01') as b
+        on a.SR_ID=b.PARTY_ACCOUNT_ID
     left join "ADW_PROD"."ADW_REFERENCE_PL"."DIM_WEEK" as c
     on b.transaction_date between WEEK_COMMENCING_DATE and WEEK_ENDING_DATE
-    where transaction_date > '2022-01-01'
-    group by 1, 2;
+    group by 1,2,3,6,7,8;
 
 
+
+//Final Output table//
+-- gives customers by week and flags (transaction)
 create or replace table bank_customers_tracking as
-select  week_num
-        ,sum(trans) as transactions
+select  week_num,
+        DN_8_wk_flag,
+        Ranking_File_Flag,
+        gol_flag
         ,sum(spend) as spend,
-       sum(countspend) as total_visits
+       sum(trans) as visits,
+       count(distinct sr_id) as Customers
 from    cc_bank_TRANS
-group by week_num
+group by 1,2,3,4
 ;
 
+// Trying to track customers by week but due to needing week it includes transactions
+// How is it done where it will update each week with the ranking file and that will give the week (not transaction week)
+Create or replace table Bank_CC_Customers1 as
+    select  count(distinct ec_id) as count,
+                    Ranking_File_Flag,
+                    gol_flag,
+                    DN_8_wk_flag,
+           week_num
+from cc_bank_TRANS group by 2,3,4,5;
 
 
-select count(*) from Bank_card_customers_in_RF;
---18254 bank customers in ranking file
+Create or replace table Bank_CC_Customers as
+    select  sum(count) as count,
+                    Ranking_File_Flag,
+                    gol_flag,
+                    DN_8_wk_flag,
+                    week_num
+from Bank_CC_Customers1 group by 2,3,4,5;
 
-
-select * from Bank_card_customers_in_RF;
-
-
-create or replace table bank_card_customers_8wk_DN_active as;
-
-
-select * from "EDWS_PREPROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_REGISTERED";  where ec_id = '50000029998783';
-
-
-
-select * from ADW_PROD.ADW_CUSTOMER_PL.DIM_LOYALTY_SERVICE_CUSTOMER limit 5;
-
-
-
-
+select count, DN_8_wk_flag from Bank_CC_Customers group by 1,2;
+select week_num from Bank_CC_Customers;
 
 --
+-- select count(*) from Bank_card_customers_in_RF;
+-- --18254 bank customers in ranking file
+--
+--
+-- select * from Bank_card_customers_in_RF;
+--
+--
+-- create or replace table bank_card_customers_8wk_DN_active as;
+--
+--
+-- select * from "EDWS_PREPROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_REGISTERED";  where ec_id = '50000029998783';
+--
+--
+--
+-- select * from ADW_PROD.ADW_CUSTOMER_PL.DIM_LOYALTY_SERVICE_CUSTOMER limit 5;
+--
+--
+--
+--
+--
+-- --
+-- -- select a.*,
+-- --        b.weeks_since_latest_activity,
+-- --        b.grouped_segmentation
+-- -- from Bank_card_customers_in_RF as a
+-- -- inner join "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_SEGMENTATION" as b
+-- -- on a.enterprise_customer_id = b.ec_id;
+-- -- -- in DIG_NECTAR_SEG
+--
+--
+--
+--
+-- -- -- those that aren't in RF
+-- -- create or replace temp table Bank_customers_not_in_RF as
+-- --     select a.*
+-- --     from bank_base a
+-- --     left join Bank_card_customers_in_RF as b on a.SR_ID=b.party_account_id
+-- --     where b.party_account_id is null;
+-- --
+-- --
+-- -- -- look at the last shop of those that are not in the RF
+-- -- create or replace temp table bank_card_not_in_RF_last_shop as
+-- --     select a.SR_ID, max(TRANSACTION_DATE) as last_trx,
+-- --        case when last_trx < DATEADD(WEEK, -26, CURRENT_DATE()) then 'more than 26w'
+-- --            when last_trx >= DATEADD(day, -5, CURRENT_DATE()) then 'recent_transaction'
+-- --     else 'probably GOL' end as flag
+-- --     from Bank_customers_not_in_RF as a
+-- --     inner join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION as b on a.SR_ID=b.PARTY_ACCOUNT_ID
+-- --     group by 1;
+--
+--
+
+
+-- create or replace temp table bank_card_customers_transactions as
+-- select a.weeks_since_latest_activity,
+--        a.grouped_segmentation,
+--        a.sr_id,
+--            a.enterprise_customer_ID,
+--           a.segment,
+--            a.hashed_loyalty_id,
+--            a.instore_accounts,
+--            a.online_accounts,
+--            a.journey_instore,
+--            a.party_account_type_code,
+--        a.DN_8_wk_flag,
+--        a.Ranking_File_Flag,
+--        a.FULL_NECTAR_CARD_NUM,
+--        case when a.ranking_file_flag = 0 then
+--                                 case when max(b.transaction_date) < DATEADD(WEEK, -26, CURRENT_DATE()) then 'more than 26w'
+--                                 when max(b.transaction_date) >= DATEADD(day, -5, CURRENT_DATE()) then 'recent_transaction'
+--                                 else 'probably GOL' end
+--            when a.ranking_file_flag = 1 then null else null end as Transaction_flag
+-- from bank_card_customers_8wk_DN_active a
+--  left join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION as b on a.SR_ID=b.PARTY_ACCOUNT_ID
+-- group by 1,2,3,4,5,6,7,8,9,10,11,12,13;
+
+
+-- create or replace temp table bank_card_customers_GOL_1 as
+-- select case when G_26WK_TOTVALUE > 100
+--     then 'GOL'
+--     else 'Not GOL'
+--     end as gol_flag,
+--        *
+-- from bank_card_customers_GOL;
+--
+-- select count(distinct SR_ID), gol_flag
+-- from bank_card_customers_GOL_1
+-- group by 2;
+--
+-- select * from bank_card_customers_GOL_1 where sr_id in (select sr_id from bank_card_customers_transactions2);
+--
+-- select count (*) from bank_card_customers_GOL_1;
+--
+--
+-- Create or replace temp table bank_card_customers_Online as
+--     select a.*,
+--            ifnull(b.gol_flag,'Not GOL') as GOL
+-- from bank_card_customers_transactions2 a
+-- left join bank_card_customers_GOL_1 b
+-- on a.sr_id = b.sr_id;
+--
+-- select * from bank_card_customers_transactions2;
+-- --19338
+-- select count(distinct sr_id), count(distinct enterprise_customer_ID), gol  from bank_card_customers_Online group by 3;
+-- -- 19338
+--
+-- select * from bank_card_customers_Online where gol_flag is not null;
+--
+-- create or replace temp table bank_card_customers_transactions as
+--     select a.ec_id, max(TRANSACTION_DATE) as last_trx
+-- from bank_card_customers_8wk_DN_active2 a
+--     inner join EDWS_PROD.PROD_EDW_SAS_ADHOC_VIEWS.VW_SHOPPING_TRANSACTION as b on a.SR_ID=b.PARTY_ACCOUNT_ID
+-- where Ranking_File_Flag = 0
+--    group by 1;
+-- --816
+--
+--
+-- -- see when the transactions have been
+-- create or replace temp table bank_card_customers_transactions1 as
+-- select *,
+--        case when last_trx < DATEADD(WEEK, -26, CURRENT_DATE()) then 'more than 26w'
+--             when last_trx >= DATEADD(day, -5, CURRENT_DATE())  then 'recent transaction'
+--            else 'probably GOL' end as transaction_flag
+-- from bank_card_customers_transactions
+-- ;
+--
+-- select count(*), transaction_flag from bank_card_customers_transactions1 group by 2;
+--
+-- create or replace temp table bank_card_customers_transactions2 as
+--     select a.*,
+--            b.transaction_flag
+-- from bank_card_customers_8wk_DN_active a
+-- left join bank_card_customers_transactions1 b
+-- on a.sr_id=b.sr_id;
+--
+-- select * from bank_card_customers_transactions2 where transaction_flag is not null;
+
+-- create or replace table bank_card_customers_8wk_DN_active as
 -- select a.*,
 --        b.weeks_since_latest_activity,
---        b.grouped_segmentation
+--        b.grouped_segmentation,
+--        case when b.weeks_since_latest_activity <= '8' then CAST('less_than_8wk' AS varchar)
+--            when b.weeks_since_latest_activity > '8' then CAST('over_8wk' AS varchar)
+--            end as DN_8_wk_flag
 -- from Bank_card_customers_in_RF as a
--- inner join "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_SEGMENTATION" as b
+-- Left join "EDWS_PROD"."PROD_CMT_CAMPAIGN_01"."DIG_NECTAR_SEGMENTATION" as b
 -- on a.enterprise_customer_id = b.ec_id;
--- -- in DIG_NECTAR_SEG
 
-
-
+-- Create or replace temp table Bank_card_customers_in_RF as
+--     select
+--            a.sr_id,
+--            b.enterprise_customer_ID,
+--            b.segment,
+--            a.hashed_loyalty_id,
+--            b.instore_accounts,
+--            b.online_accounts,
+--            c.journey_instore,
+--            b.party_account_type_code,
+--            a.FULL_NECTAR_CARD_NUM,
+--            case when b.party_account_id is not null then 1
+--                when b.party_account_id is null then 0 end as Ranking_File_Flag
+-- from bank_base as a
+-- left outer join EDWS_PROD.PROD_CMT_CAMPAIGN_01.RANKING_FILE_SR_NODUPES as b on b.PARTY_ACCOUNT_ID=a.SR_ID
+--     left join TD_REPORTING.TD_EXPERIMENTAL_JOURNEYS as c
+--     on b.enterprise_customer_ID = c.enterprise_customer_ID;
